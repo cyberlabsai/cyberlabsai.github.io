08f98929534b2b777f8826bf5f81c4649b21f7a2
Sabrina <sabrinaangelimsampaio@gmail.com>
2019-06-05 19:15:53 -0300

    Adding analytics

--- assets/theme/js/amplitude.js
+++ assets/theme/js/amplitude.js
@@ -5,13 +5,13 @@ function setUserProp(identifyObj) {
   amplitude.getInstance().identify(identifyObj);
 }
 function setAmplitudeEvent(eventName) {
-  var clickedArrowButton = new amplitude.Identify().set('clicked on next button', '1');
+  var clickedNextButton = new amplitude.Identify().set('clicked on next button', '1');
   var clickedTryHereButton = new amplitude.Identify().set('clicked on teste aqui button', '1');
   var clickedInSightNowButton = new amplitude.Identify().set('clicked on insight now button', '1');
   var clickedLogisticsButton = new amplitude.Identify().set('clicked on logistica button', '1');
   var clickedFacialRecognitionButton = new amplitude.Identify().set('clicked on reconhecimento facial button', '1');
   var clickedHealthButton = new amplitude.Identify().set('clicked on health button', '1');
-  var copiedCyberlabsEmail = new amplitude.Identify().set('copied cyberlabs email', '1');
+  var clickedAboutUsButton = new amplitude.Identify().set('clicked on sobre nós button', '1');
   var clickedlinkedinButton = new amplitude.Identify().set('clicked on linkedin button', '1');
   var clickedFacebookButton = new amplitude.Identify().set('clicked on facebook button', '1');
   var clickedInstagramButton = new amplitude.Identify().set('clicked on instagram button', '1');
